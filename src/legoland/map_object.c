0dcb68cb559f49577121d5f9f83569483a6037df
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-09-26 08:22:21 +0200

    map_object: match PlayAppropriateBuildEffect (rand index via local)

--- src/legoland/map_object.c
+++ src/legoland/map_object.c
@@ -2901,29 +2901,31 @@ LEGO_EXPORT void PlayAppropriateBuildEffect(struct ObjClass *obj, int *coords) {
     unsigned int flags;
     int *param;
     int buf[4];
+    int i;
 
-    param = buf;
-    if (coords == 0) {
-        param = 0;
-    } else {
+    if (coords != 0) {
         buf[0] = 2;
         buf[2] = coords[0];
         buf[3] = coords[1];
+        param = buf;
+    } else {
+        param = 0;
     }
     flags = ((struct MapObject *)obj)->flags;
-    if ((flags & 0x40000) != 0) {
+    if (flags & 0x40000) {
         PlayInstanceOfSample(DAT_004b9230[3], 0, 1, param);
         return;
     }
-    if ((flags & 0x80000) == 0) {
-        PlayInstanceOfSample(DAT_004b9230[0], 0, 1, param);
+    if (flags & 0x80000) {
+        if (flags & 0x200000) {
+            PlayInstanceOfSample(DAT_004b9230[0], 0, 1, param);
+            return;
+        }
+        i = rand() % 5 + 3;
+        PlayInstanceOfSample(DAT_004b9230[i * 3], 0, 1, param);
         return;
     }
-    if ((flags & 0x200000) != 0) {
-        PlayInstanceOfSample(DAT_004b9230[0], 0, 1, param);
-        return;
-    }
-    PlayInstanceOfSample(DAT_004b9230[(rand() % 5 + 3) * 3], 0, 1, param);
+    PlayInstanceOfSample(DAT_004b9230[0], 0, 1, param);
 }
 
 // FUNCTION: LEGOLAND 0x00462dd0
