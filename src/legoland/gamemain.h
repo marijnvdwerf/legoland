a48a71af61ca19fe44ea295f57e69b886d566103
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-09-26 10:49:25 +0200

    Move the event/query list functions (0x477680-0x477790) into gamemain
    They work the same DAT_00668fc0 list as gamemain's first function and sit
    directly before it, so they belong to gamemain's object.

--- src/legoland/gamemain.h
+++ src/legoland/gamemain.h
@@ -1,6 +1,15 @@
 #pragma once
 
+struct EventNode;
 struct Point;
+struct QueryNode;
+
+int FUN_00477680(int a, int b);
+void FUN_004776c0(struct QueryNode *node);
+void FUN_004776e0(struct EventNode *node);
+struct QueryNode *FUN_00477730(struct QueryNode *ctx);
+void FUN_00477760(struct QueryNode *ctx);
+void FUN_00477790(struct EventNode *param_1);
 
 void FUN_004779d0(struct Point *p);
 void FUN_00477bd0(int x, int y, int a, int b);
